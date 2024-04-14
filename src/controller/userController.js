586d08afa6e9fd21664c988df35e7d6e5e6267f8
TomasMarriscurrena <tomasmarriscurrena@gmail.com>
2024-04-14 16:29:57 -0300

    new coomit

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,4 @@
-const {createUserService, getUserbyUsernameService} = require("../services/userService");
+const {createUserService, getUserbyUsernameService , updateUserService, deleteUserService}= require("../services/userService");
 
 const validateStatus = async (res, user, status) => {
     if(user.status === status) 
@@ -17,16 +17,25 @@ const createUserController = async (req, res) => {
     res.status(201).json(user);
 }
 
-//const updateUserController = async (req, res) => {
-//    const updatedUser = await updateUserService(req);
+const updateUserController = async (req, res) => {
+    try {
+        const username = req.params.username; 
+        const updatedUser = await updateUserService(username, req.body); 
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
 
-//    res.json(updatedUser);
-//}
+const deleteUserController = async (req, res) => {
+    try {
+        const username = req.params.username; 
+        const deletedUser = await deleteUserService(username); 
+        res.status(200).json(deletedUser);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
 
-//const deleteUserController = async (req, res) => {
-//    const deletedUser = await deleteUserService(req);
 
-//    res.json(deletedUser);
-//}
-
-module.exports = {createUserController, getUserbyUsernameController};
+module.exports = {createUserController, getUserbyUsernameController,updateUserController,deleteUserController};
